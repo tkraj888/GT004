197634007b24f9a1ef82668c20cb8ce7a6cc3f2c
AshishTayade <ashishtayade@gtasterix.com>
2025-02-05 15:45:54 +0530

    ok

--- src/main/java/com/spring/jwt/stockTransaction/StockTransactionController.java
+++ src/main/java/com/spring/jwt/stockTransaction/StockTransactionController.java
@@ -1,7 +1,13 @@
 package com.spring.jwt.stockTransaction;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/stockTransaction")
 public class StockTransactionController {
+
+    @Autowired
+    private StockTransactionService
 }
